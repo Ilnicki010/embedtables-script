eb6c4484378286050f72d9028426802a6be057e0
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-11-08 13:17:05 +0100

    fix env var

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { client, q } from "../config/db";
 export const getProjectById = (id) => {
-  return fetch(`${LAMBDA_ENDPOINT}/projects/data/${id}`)
+  return fetch(`${process.env.LAMBDA_ENDPOINT}/projects/data/${id}`)
     .then((res) => res.json())
     .then((res) => res);
 };
